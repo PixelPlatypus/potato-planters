da875c51912c9af562c9e2484f792268607400a3
CaffeineDuck <hello@samrid.me>
2023-10-07 17:51:31 +0545

    chore: remove unnecessary auto generated comments

--- src/components/TextSection.jsx
+++ src/components/TextSection.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import { Text } from "@react-three/drei";
+import { fadeOnBeforeCompileFlat } from "../utils/fadeMaterial";
 
-function TextSection() {
+export const TextSection = ({ title, subtitle, ...props }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <group {...props}>
+      {!!title && (
+        <Text
+          color="white"
+          anchorX={"left"}
+          anchorY="bottom"
+          fontSize={0.52}
+          maxWidth={2.5}
+          lineHeight={1}
+          font={"./fonts/DMSerifDisplay-Regular.ttf"}
+        >
+          {title}
+          <meshStandardMaterial
+            color={"white"}
+            onBeforeCompile={fadeOnBeforeCompileFlat}
+          />
+        </Text>
+      )}
 
-export default TextSection
+      <Text
+        color="white"
+        anchorX={"left"}
+        anchorY="top"
+        fontSize={0.2}
+        maxWidth={2.5}
+        font={"./fonts/Inter-Regular.ttf"}
+      >
+        {subtitle}
+        <meshStandardMaterial
+          color={"white"}
+          onBeforeCompile={fadeOnBeforeCompileFlat}
+        />
+      </Text>
+    </group>
+  );
+};
