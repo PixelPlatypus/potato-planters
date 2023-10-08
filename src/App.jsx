5c3571a4af7fe2c50ca584dc74a62a2bb35449af
zraffens <abhinavacharya486@gmail.com>
2023-10-08 17:20:27 +0545

    chore: remote routes var

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,10 @@ import Explore from "./pages/Explore";
 import Landing from "./pages/Landing";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Route, Routes, useLocation } from "react-router-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-// ...
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Landing />,
-  },
-  {
-    path: "/explore",
-    element: <Explore />,
-  },
-]);
 
 const App = () => {
-  const  location = useLocation();
+  const location = useLocation();
   return (
     <TransitionGroup>
       <CSSTransition key={location.key} classNames="fade" timeout={400}>
